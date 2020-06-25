31fc3ae20cbba7b786ca00419197eaa91da59f73
Suhyun <aodlfduddj@gmail.com>
2020-06-25 18:56:56 +0900

    Init commit

---
+++ src/utils/users.js
@@ -0,0 +1,59 @@
+const users = []
+
+// addUser, removeUser, getUser, getUsersInRoom
+
+const addUser = ({ id, username, room }) => {
+    // Clean the data
+    username = username.trim().toLowerCase()
+    room.trim().toLowerCase()
+
+    // validate the data
+    if (!username || !room) {
+        return {
+            error: 'Username and room are required!'
+        }
+    }
+
+    // Chekc for existing user
+    const existingUser = users.find((user) => {
+        return user.room === room && user.username === username
+    })
+
+    // Validate username
+    if (existingUser) {
+        return {
+            error: 'Username is in use!'
+        }
+    }
+
+    // Store user
+    const user = {id, username, room }
+    users.push(user)
+    return { user }
+}
+
+const removeUser = (id) => {
+    const index = users.findIndex((user) => user.id === id)
+
+    if (index !== -1) {
+        return users.splice(index, 1)[0]
+    }
+}
+
+const getUser = (id) => {
+    return users.find((user) => user.id === id)
+}
+
+const getUsersInRoom = (room) => {
+    room = room.trim().toLowerCase()
+    usersInRoom = users.filter((user) => user.room === room)
+
+    return usersInRoom
+}
+
+module.exports = {
+    addUser,
+    removeUser,
+    getUser,
+    getUsersInRoom
+}
